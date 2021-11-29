c3600ace6721a959dca797535784985b0f1e3431
Yuri Chiba <yurizin163@gmail.com>
2021-11-29 11:12:48 -0300

    Controller usuario finalizado

--- Back-End/senai_roman_webAPI/senai_roman_webAPI/Controllers/LoginController.cs
+++ Back-End/senai_roman_webAPI/senai_roman_webAPI/Controllers/LoginController.cs
@@ -1,16 +1,68 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using senai_roman_webAPI.Domains;
+using senai_roman_webAPI.Interfaces;
 using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 
 namespace senai_roman_webAPI.Controllers
 {
-    public class LoginController : Controller
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
     {
-        public IActionResult Index()
+        private IUsuarioRepository _Repository { get; set; }
+
+        public LoginController()
         {
-            return View();
+            _Repository = new IUsuarioRepository();
+        }
+
+        /// <summary>
+        /// Método responsável por fazer o login na api
+        /// </summary>
+        /// <returns>Lista apenas o objeto selecionado</returns>
+        [HttpPost()]
+        public IActionResult Login(LoginViewModel login)
+        {
+            try
+            {
+                Usuario usuarioBuscado = _Repository.Logar(login.email, login.senha);
+
+                if(usuarioBuscado == null)
+                {
+                    return BadRequest($"E-mail ou senha inválidos!");
+                }
+                var minhasClaims = new[]
+                {
+                    new Claim(JwtRegisteredClaimNames.Email, usuarioBuscado.EmailUsuario),
+                    new Claim(ClaimTypes.Role, usuarioBuscado.IdTipoUsuario.ToString()),
+                    new Claim("role", usuarioBuscado.IdTipoUsuario.ToString())
+                }
+                var key = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes("roman-chave-autenticacao"));
+                var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+                var meuToken = new JwtSecurityToken(
+                    issuer: "senai_roman.webAPI",
+                    audience: "senai_roman.webAPI"
+                    claims: minhasClaims,
+                    expires: DateTime.Now.AddHours(2),
+                    signingCredentials: creds
+                    );
+
+                return Ok(new
+                {
+                    token = new JwtSecurityTokenHandler().WriteToken(meuToken)
+                });
+            }
+            catch(Exception ex)
+            {
+                return BadRequest(ex);
+            }
         }
     }
 }
